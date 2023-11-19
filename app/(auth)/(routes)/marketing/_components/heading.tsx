7ca7f378a546abaf544ca67d3a12a89dac0f3c9e
umika <442491636@qq.com>
2023-11-19 14:17:20 +0800

    feat

---
+++ app/(auth)/(routes)/marketing/_components/heading.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export const Heading = () => {
+    return(
+        <div className="max-w-3xl space-y-4">
+            <h1>You Ideas, Documents, & Plans. Unified. Welcome to the<span
+            className="underline">
+                Jotion
+                </span></h1>
+        </div>
+    )
+}
---
+++ app/(auth)/(routes)/marketing/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Heading } from "./_components/heading";
+
+const MarktingPage = () => {
+    return (
+        <div className="min-h-full flex flex-col">
+          <div className="flex flex-col items-center justify-center
+          md:justify-start text-start gap-y-8 flex-1 px-6 pb-10">
+            <Heading/>
+          </div>
+        </div>
+      )
+}
+ 
+export default MarktingPage;
